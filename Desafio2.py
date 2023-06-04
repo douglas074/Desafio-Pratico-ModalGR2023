f1bf982e8d53f1a0fdd9bb4a0e553854de5b7348
Douglas Guilherme <douglas.guilherme0704@gmail.com>
2023-06-04 13:21:16 -0300

    Refatoração e adição de verificação na escolha de nova conversão

--- Desafio2.py
+++ Desafio2.py
@@ -1,13 +1,14 @@
 import os
 
 
-def clear_console():
+def clear_console():  # Possui a funcionalidade de limpar a tela do console
     if os.name == 'nt':  # Windows
         os.system('cls')
     else:  # Unix/Linux/Mac
         os.system('clear')
 
 
+# Possui a funcionalidade de relacionar a nota musical com o algarismo romano correspondente.
 def get_degrees(notes):
     degree = {
         'dó': 'I',
@@ -23,24 +24,41 @@ def get_degrees(notes):
         'fa': 'IV',
         'la': 'VI',
     }
+
+    # Retorna o valor romano correspondente, transforma letras maiúsculas em minúsculas para evitar erros de verificação.
     return [degree.get(note.lower(), 'nota não encontrada') for note in notes]
 
 
-clear_console()
+clear_console()  # Limpar console
+
 aux = '1'
-while aux == '1' or aux == 'sim':
+
+# Loop de verificação se a escolha de repetição é positivo
+while aux == '1' or aux == 'sim' or aux == 's':
     clear_console()
 
     print('Conversor de notas musicais;\n')
+    musical_notes = []
     notes = []
     a = 1
+
+    # Loop para receber as notas musicais
     for i in range(10):
         print(f"Por favor, informe a {a}ª nota musical: ")
         value = input()
+        musical_notes.append(value)
         notes.append(value)
         a += 1
 
     print('\nOs valores da notas musicais informadas são respectivamente: \n',
+          musical_notes, '\n',
           get_degrees(notes))
+
     aux = input(
-        '\nDeseja fazer uma nova conversão?\n1-  sim\n2-  nao\n Escolha uma das opções acima:  ')
+        '\nDeseja fazer uma nova conversão?\n  1-  sim\n  2-  nao\n  Escolha uma das opções acima:  ')
+
+    # Loop de verificação caso a opção escolhida não exista
+    while aux != 'sim' and aux != 'nao' and aux != 's' and aux != 'n' and aux != '1' and aux != '2':
+        print('\nOpção não encontrada, por favor tente novamente')
+        aux = input(
+            'Deseja fazer uma nova conversão?\n  1-  sim\n  2-  nao\n  Escolha uma das opções acima:  ')
